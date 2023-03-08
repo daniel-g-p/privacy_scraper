d1d33321bde1c2e3d138e513c0d2ecda0b72f99c
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2023-03-08 11:01:09 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+export default {
+  privacy_policy_link_keywords: ["privacy", "datenschutz"],
+  flagged_cookie_names: ["_ga"],
+  flagged_privacy_policy_keywords: [
+    { label: "Google Analytics", keywords: ["Google Analytics"] },
+    { label: "Google Fonts", keywords: ["Google Fonts"] },
+    { label: "Privacy Shield", keywords: ["Privacy Shield"] },
+  ],
+  expected_privacy_policy_keywords: [
+    {
+      label: "DPO",
+      keywords: [
+        "Datenschutzbeauftragter",
+        "Datenschutzbeauftragte",
+        "Data Privacy Officer",
+        "Data Protection Officer",
+      ],
+    },
+  ],
+};
